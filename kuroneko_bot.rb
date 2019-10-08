9014db8996695b5a3bd83354bc850f0c8a44ded4
sasasoni <sasasoni1119@gmail.com>
2019-10-09 00:07:33 +0900

    dice_botをkuroneko_botに変更して翻訳機能を追加

--- kuroneko_bot.rb
+++ kuroneko_bot.rb
@@ -3,19 +3,30 @@ require_relative 'fortune.rb'
 require_relative 'lunch_selection.rb'
 require_relative 'dice.rb'
 require_relative 'member_selection.rb'
+require_relative 'translator.rb'
 
-class DiceBot < SlackRubyBot::Bot
+class KuronekoBot < SlackRubyBot::Bot
   DICE_LIST = [ Fortune.new, LunchSelection.new, MemberSelection.new, Dice.new ]
   def self.possibilities(dice_type, client, data)
     accepted_dice = DICE_LIST.find{|dice| dice.accept?(dice_type)}
     accepted_dice.possibilities(client, data)
   end
 
-  match /サイコロ(\s+(?<dice_type>.+))?/ do |client, data, match|
+  def self.translator
+    @translator ||= Translator.new
+  end
+
+  match /サイコロ(\s|　)+(?<dice_type>.+)/ do |client, data, match|
     dice_type = match[:dice_type]
     client.say(text: possibilities(dice_type, client, data).sample,
                 channel: data.channel)
   end
+
+  match /翻訳(\s|　)+(?<text>.+)/ do |client, data, match|
+    text = match[:text]
+    client.say(text: "`#{text}`を訳すと", channel: data.channel)
+    client.say(text: translator.translate(text), channel: data.channel)
+  end
 end
 
-DiceBot.run
+KuronekoBot.run
